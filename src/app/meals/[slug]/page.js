2c5e636675b81667ef0ce4ab2a535e982004891f
Vikash <vikashparjapati59@gmail.com>
2024-09-04 00:13:39 +0530

    cart

--- src/app/meals/[slug]/page.js
+++ src/app/meals/[slug]/page.js
@@ -25,7 +25,7 @@ const Meal = () => {
         <main className="p-8 bg-gray-100 min-h-screen">
           <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
             <div className="relative">
-              <img src={meal.image} alt={meal.name} className="w-full h-64 object-cover" />
+              <img src={meal.image} alt={meal.name} className="w-full h-full object-cover" />
             </div>
             <div className="p-6">
               <h1 className="text-3xl font-bold text-gray-800">{meal.name}</h1>
@@ -33,8 +33,9 @@ const Meal = () => {
               <p className="text-2xl font-bold text-blue-600 mt-4">${meal.price}</p>
               <p className="text-gray-700 mt-4">{meal.description}</p>
               <div className="mt-6 flex justify-between">
-                <Button className="bg-blue-500 text-whit  hover:bg-blue-600 focus:outline-none ">Add to Cart
-                </Button>
+                <Link href="/cart" >               <Button  className="bg-blue-500 text-whit  hover:bg-blue-600 focus:outline-none ">Add to Cart
+                </Button></Link>
+ 
                 <Link href="/meals">
                   <p className="px-4 py-2 bg-gray-300 text-gray-800 rounded-md shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50">
                     Back to List
