1f721cce336e98eac02b1910484018a7ecbb57f5
Vikash <vikashparjapati59@gmail.com>
2024-09-15 19:46:18 +0530

    api for location dropdown

---
+++ src/app/api/mealsname/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+import { Login } from "@/lib/models/products";
+import { loginstr } from "@/lib/models/db";
+
+export async function GET(req) {
+  try {
+    await mongoose.connect(loginstr);
+    var result = await Login.find();
+    result=result.map((item)=>(
+     item?.city
+     
+    ))
+    result=[...new Set(result)];
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("GET request error:", error);
+    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,34 @@
-"use client"
+"use client";
 
 export default function Home() {
   return (
-    <main >
-  
-      <div className="bg-[url('/loginbg.jpeg')] bg-cover bg-center h-screen  flex items-center justify-center">
+    <main>
+      {/* <div className="bg-[url('/loginbg.jpeg')] bg-cover bg-center h-[200px] flex items-center justify-center">
+        <div className="flex flex-col md:flex-row items-center w-full max-w-4xl p-4 text-black ">
+          <form className="flex items-center border-none   rounded-md shadow-md">
+            <select
+              id="cars"
+              name="cars"
+              className="bg-white border rounded-l p-2 focus:outline-none  w-full"
+            >
+              <option value="" disabled selected>
+                Location
+              </option>
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="opel">Opel</option>
+              <option value="audi">Audi</option>
+            </select>
+          </form>
 
-      
-      </div>
-      </main>
+
+          <input
+            type="text"
+            placeholder="Search meal and restorent"
+            className="border  rounded-r p-2 bg-white text-black focus:outline-none  w-full sm:w-1/2"
+          />
+        </div>
+      </div> */}
+    </main>
   );
 }
